efa5b717418a6009720adcc4b134052e6e9cd872
wangyuanhao <wyhhrm@163.com>
2019-03-31 14:16:27 +0800

    init project

---
+++ RelationCaculate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RelationCaculate
+//
+//  Created by mac on 2019/3/31.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
